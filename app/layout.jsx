7a0d90eed197eae34353d1ca93f3f6b0cd9fdeb4
madhusailesh <madhusaileshsasamal998@gmail.com>
2026-08-02 11:52:21 +0530

    Refactor code structure for improved readability and maintainability

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,11 @@ import "./globals.css";
 export const metadata = {
   title: "E-Cell VSSUT",
   description: "A Techno-Management Club of VSSUT Burla",
+  icons: {
+    icon: "/toplogo.png", // Yeh browser tab ke top par favicon set kar dega
+    shortcut: "/toplogo.png",
+    apple: "/toplogo.png",
+  },
 };
 
 export default function RootLayout({ children }) {
